754a715cbd1cb0b2589f95c44cf8e4611450df70
Pelep <moncadaphil@gmail.com>
2023-03-21 14:56:50 -0700

    Design Adjustments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,25 +6,25 @@ function Home() {
 
     useEffect(() => {
         fetch('https://dummyjson.com/products')
-        .then(async res  => await res.json())
-        .then(data => setPosts(data.products))
+            .then(async res => await res.json())
+            .then(data => setPosts(data.products))
 
-    },[])
+    }, [])
 
 
     return (
         <div className="container">
             {posts.map((post, i) =>
-            <div className = {"card"} key = {i}>
-                <h1>{post.title}</h1>
-                <img src={post.images[0]} alt="" />
-            <div className = {"price"} key = {i}>
-            <h3>{post.description}</h3>
-             <h2>Price: {post.price}</h2>
-            <div className = {"discountTag"}><h2>Discount: {post.discountPercentage}%</h2>
-            </div>
-            </div>
-            </div>     
+                <div className={"card"} key={i}>
+                    <h1>{post.title}</h1>
+                    <img src={post.images[0]} alt="" />
+                    <div className={"price"} key={i}>
+                        <h3>{post.description}</h3>
+                        <h2>Price: {post.price}</h2>
+                        <div className={"discountTag"}><h1>Discount: {post.discountPercentage}%</h1>
+                        </div>
+                    </div>
+                </div>
             )}
         </div>
 
