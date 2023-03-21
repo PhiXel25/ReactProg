601285671aea156b6b235ee89f10e1b73befc367
Pelep <moncadaphil@gmail.com>
2023-03-21 16:31:53 -0700

    Design

--- src/components/PrdctCard.jsx
+++ src/components/PrdctCard.jsx
@@ -1,9 +1,17 @@
 
-function PrdctCard() {
-
+function PrdctCard(post) {
     return (
-        <>
-        </>
+
+        <a href={"/products/" + post.post.id} className="card">
+            <h1>{post.post.title}</h1>
+            <img src={post.post.images[0]} alt="" />
+            <div className="price">
+                <h3>{post.post.description}</h3>
+                <h2>Price: {post.post.price}</h2>
+                <div className="discountTag"><h1>Discount: {post.post.discountPercentage}%</h1>
+                </div>
+            </div>
+        </a>
 
     )
 }
